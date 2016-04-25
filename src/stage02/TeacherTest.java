557c838c8c2e21b15a5f2a43948214fbd8fdcc06
zxwtry <zxwtry@qq.com>
2016-04-25 21:38:46 +0800

    添加测试，增加类功能。

---
+++ src/stage02/TeacherTest.java
@@ -0,0 +1,27 @@
+package stage02;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+
+public class TeacherTest {
+	@Test
+	public void teacherTest01() {
+		Teacher teacher01 = new Teacher();
+		teacher01.setId(1);
+		teacher01.setName("teacher01Name");
+		teacher01.setTitle("teacher01Title");
+		
+		Configuration configuration = new Configuration();
+		SessionFactory sessionFactory = configuration.configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.getTransaction();
+		session.save(teacher01);
+		session.getTransaction().commit();
+		session.clear();
+		sessionFactory.close();
+		
+		
+	}
+}
